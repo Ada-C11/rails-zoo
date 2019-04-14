f00c8c4036b53fd7d98bcbce08828b3fb6d1c44b
Bitaman <b.amani.s@gmail.com>
2019-04-14 08:09:58 -0700

    added the show action, made the erb file and verified

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,9 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+  def show
+    animal_id = params[:id].to_i
+    @animal = Animal.find_by(id: animal_id)
+  end
 end
